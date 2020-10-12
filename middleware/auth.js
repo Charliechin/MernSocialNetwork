60339a53b433289dd7407611a8607d832f2c7fcc
Carlos Diaz <charliedn@gmail.com>
2020-10-12 18:03:02 +0100

    Add Auth routes and user login

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+
+module.exports = (req, res, next) => {
+  // Get Token from Header
+  const token = req.header('x-auth-token');
+
+  // Check if not token
+  if (!token) {
+    // not authorized
+    return res
+      .status(401).json({ msg: "No token, auth denied" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.get('jwtToken'));
+
+    req.user = decoded.user;
+    next();
+
+  } catch (error) {
+    res.status(401).json({ msg: "Token is not Valid" });
+
+  }
+}

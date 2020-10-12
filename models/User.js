6ca6350af6afae591ac35e5ef1bb335c4ea0f8a9
Carlos Diaz <charliedn@gmail.com>
2020-10-12 15:54:05 +0100

    Create User Model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const moongose = require('mongoose');
+const UserSchema = new moongose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const User = moongose.model('user', UserSchema);
+module.exports = User;

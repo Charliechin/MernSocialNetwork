6a23cc32090267c196da072483fbb11dd6f57ee6
Carlos Diaz <charliedn@gmail.com>
2020-10-15 14:40:41 +0100

    Add actions to login (Redux)

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types'
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from './types'
 import setAuthToken from '../utils/setAuthToken';
 // load User
 export const loadUser = () => async dispatch => {
@@ -40,13 +40,43 @@ export const register = ({ name, email, password }) => async dispatch => {
 
 
   } catch (error) {
-    debugger
     const errors = error.response.data.errors;
     if (errors) {
       errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
     }
     dispatch({
       type: REGISTER_FAIL
+    })
+    console.error(error.message);
+  };
+}
+
+
+// Login User
+// Register User
+export const login = (email, password) => async dispatch => {
+  const config = {
+    headers: { 'Content-Type': 'application/json' }
+  }
+
+  const body = JSON.stringify({ email, password });
+
+  try {
+    const res = await axios.post('/api/auth', body, config);
+
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data
+    });
+
+
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: LOGIN_FAIL
     })
     console.error(error.message);
   };
